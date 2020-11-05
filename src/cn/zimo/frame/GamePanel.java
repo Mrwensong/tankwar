bf9cdeccd58001204ee69b2f98f00893e68943b7
Mrwensong <2739934654@qq.com>
2020-11-05 15:27:13 +0800

    电脑坦克数量修改

--- src/cn/zimo/frame/GamePanel.java
+++ src/cn/zimo/frame/GamePanel.java
@@ -49,7 +49,7 @@ public class GamePanel extends JPanel implements KeyListener {
 	private List<Bullet> bullets;// 所有子弹集合
 	private volatile List<Tank> allTanks;// 所有坦克集合
 	private List<Tank> botTanks;// 电脑坦克集合
-	private static final int botCount = 30;// 电脑坦克总数
+	private int botCount = 20+level;// 电脑坦克总数
 	private int botReadyCount = botCount;// 准备出场的电脑坦克总数
 	private int botSurplusCount = botCount;// 电脑坦克剩余量
 	private int botMaxInMap = 6;// 场上最大电脑坦克数
